2f988633f1cac9eaf98568d21185d045deed7820
Atharv <atharvbhandare2001@gmail.com>
2021-11-04 12:06:11 +0530

    modified components/ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-  return <h2>ExpenseItem</h2>;
+  return (
+    <div>
+      <div>4 November 2021</div>
+      <div>
+        <h2>Mobile Insurance</h2>
+        <div>$100</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
