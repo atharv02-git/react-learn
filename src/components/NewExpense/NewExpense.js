f4925430dd64be54bf97d9b0d060e64bda5ac6ba
Atharv <atharvbhandare2001@gmail.com>
2021-11-14 19:17:59 +0530

    Adding conditional statements

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,36 @@
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [ShowForm, setShowForm] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setShowForm(false);
+  };
+
+  const showFormHandler = () => {
+    setShowForm(true);
+  };
+
+  const stopShowFormHandler = () => {
+    setShowForm(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!ShowForm && <button onClick={showFormHandler}>Add New Expense </button>}
+      {ShowForm && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopShowFormHandler}
+        />
+      )}
     </div>
   );
 };
