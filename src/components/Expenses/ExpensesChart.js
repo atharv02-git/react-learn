8ad2d84ddee3fb69b5a20a9e0c38243b0ff25c4a
Atharv <atharvbhandare2001@gmail.com>
2021-11-16 19:38:35 +0530

    fixing string concatenation bug

--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -19,6 +19,7 @@ const ExpensesChart = (props) => {
   for (const expense of props.expenses) {
     const expenseMonth = expense.date.getMonth(); // starting at 0 => January => 0
     chartDataPoints[expenseMonth].value += expense.amount;
+    // console.log(chartDataPoints[expenseMonth].value);
   }
 
   return <Chart dataPoints={chartDataPoints} />;
